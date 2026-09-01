a2dfb39ecbc9d7d81c0960218d61db3a08e89cd4
Mariskal19 <Mariskal19@users.noreply.github.com>
2026-09-01 19:58:29 +0200

    Fix missing AlertDialog import in ConfigurationActivity

--- app/src/main/java/com/evchargecalculator/ConfigurationActivity.java
+++ app/src/main/java/com/evchargecalculator/ConfigurationActivity.java
@@ -1,6 +1,7 @@
 package com.evchargecalculator;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
