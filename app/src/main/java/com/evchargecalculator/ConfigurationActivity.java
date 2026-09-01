74053db83ac43eaa2c951fe8db056b980f5f0728
Mariskal19 <Mariskal19@users.noreply.github.com>
2026-09-01 21:44:44 +0200

    Fix ConfigurationActivity missing Intent import

--- app/src/main/java/com/evchargecalculator/ConfigurationActivity.java
+++ app/src/main/java/com/evchargecalculator/ConfigurationActivity.java
@@ -3,6 +3,7 @@ package com.evchargecalculator;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
