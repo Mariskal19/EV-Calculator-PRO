4b59857a8d430f2e86839018da5f58409a010f96
Mariskal19 <Mariskal19@users.noreply.github.com>
2026-09-10 00:27:29 +0200

    catalog: include pass22 in generator

--- tools/generate_catalog.py
+++ tools/generate_catalog.py
@@ -16,7 +16,7 @@ SOURCES = [
     'vehicle_market_additions_pass16.json', 'vehicle_market_additions_pass17.json',
     'vehicle_market_additions_pass18.json', 'vehicle_market_additions_pass19.json',
     'vehicle_market_additions_pass20.json', 'vehicle_market_additions_pass21.json',
-    'research_mini_ES_2024_2026.json',
+    'vehicle_market_additions_pass22.json', 'research_mini_ES_2024_2026.json',
 ]
 OUTPUT = ROOT / 'catalog_es_2024_2026.json'
 
